6d12b092171d4365cd62f7b4f6ca2d7b8a5f8e16
Diana Bolocan <DianaBolocan@users.noreply.github.com>
2019-03-04 21:07:48 +0200

    Fixed sum bug

--- Tema2/tema2.py
+++ Tema2/tema2.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 
-# TO DO: fix the np.sum in get_matrix_lu (it doesn't generate correctly the numbers) 
 def get_matrix_a(path):
     size = None
     a = list()
@@ -22,51 +21,31 @@ def get_matrix_b(path):
     return np.reshape(b, (len(b), 1))
 
 
-def get_sum_list(step, i, lu, col=True):
-    sum_list = list()
-    k = 0
-    while k < i:
-        if col:
-            sum_list.append(lu[i][k] * lu[k][step])
-        else:
-            sum_list.append(lu[step][k] * lu[k][i])
-        k += 1
-    return sum_list
-
-
 def get_matrix_lu(size, a):
     lu = np.zeros((size, size))
     for step in range(size):
-        # Won't generate the right list
-        print(step)
         i = 0
         while i <= step:
-            print("\t:", i)
             if i > step - 1:
-                lu[step][i] = a[step][i] - np.sum([lu[step][k] * lu[k][i] for k in range(i - 1)])
-                print("l{}{} = {} - {}".format(step, i, a[step][i], np.sum([lu[step][k] * lu[k][i] for k in range(i)])))
-                print([lu[step][k] * lu[k][i] for k in range(i)])
-                print(get_sum_list(step, i, lu, col= False))
+                lu[step][i] = a[step][i] - np.sum([lu[step][k] if k == i else lu[step][k] * lu[k][i] for k in range(i)])
             else:
                 # compute elem col. step U
-                lu[i][step] = (a[i][step] - np.sum([lu[i][k] * lu[k][step] for k in range(i - 1)])) / lu[i][i]
-                print("u{}{} = {} - {}".format(i, step, a[i][step], np.sum([lu[i][k] * lu[k][step] for k in range(i)])))
-                print([lu[i][k] * lu[k][step] for k in range(i)])
-                print(get_sum_list(step, i, lu))
+                lu[i][step] = (a[i][step] - np.sum([lu[i][k] * lu[k][step] for k in range(i)])) / lu[i][i]
                 # compute elem. line step L
-                lu[step][i] = a[step][i] - np.sum([lu[step][k] * lu[k][i] for k in range(i - 1)])
-                print("l{}{} = {} - {}".format(step, i, a[step][i], np.sum([lu[step][k] * lu[k][i] for k in range(i)])))
-                print([lu[step][k] * lu[k][i] for k in range(i)])
-                print(get_sum_list(step, i, lu, col= False))
+                lu[step][i] = a[step][i] - np.sum([lu[step][k] if k == i else lu[step][k] * lu[k][i] for k in range(i)])
             i += 1
     return lu
 
 
-def compute_x(size, eps, a, b):
+def get_det_a(lu, size):
+    return np.product([lu[i][i] for i in range(size)])
+
+
+def compute_x_lu(size, eps, a, b):
     return
 
 
 a = get_matrix_a("matrix_A.txt")
 b = get_matrix_b("matrix_B.txt")
-compute_x(3, 3, a, b)
-print(get_matrix_lu(3, a))
+lu = get_matrix_lu(3, a)
+print(get_det_a(lu, 3))
