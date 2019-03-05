819ad818c29ab30466e2b4ea8d17f30444e4a705
ClaudiuIoanMaftei <claudiu.ioan.maftei@gmail.com>
2019-03-05 16:47:20 +0200

    Update tema2.py

--- Tema2/tema2.py
+++ Tema2/tema2.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+global epsilon
+epsilon=10**-4
+
 
 def get_matrix_a(path):
     size = None
@@ -30,6 +33,7 @@ def get_matrix_lu(size, a):
                 lu[step][i] = a[step][i] - np.sum([lu[step][k] if k == i else lu[step][k] * lu[k][i] for k in range(i)])
             else:
                 # compute elem col. step U
+                if abs(lu[i][i]) <= epsilon: print("Division by 0 at compute elem. col. step U, ("+str(i),str(i)+")"); return None
                 lu[i][step] = (a[i][step] - np.sum([lu[i][k] * lu[k][step] for k in range(i)])) / lu[i][i]
                 # compute elem. line step L
                 lu[step][i] = a[step][i] - np.sum([lu[step][k] if k == i else lu[step][k] * lu[k][i] for k in range(i)])
@@ -41,12 +45,13 @@ def get_det_a(lu, size):
     return np.product([lu[i][i] for i in range(size)])
 
 
-def compute_x_lu(size, eps, a, b):
+def compute_x_lu(size, a, b):
     lu = get_matrix_lu(size, a)
     x_lu = np.zeros(size)
     y = np.zeros(size)
     # compute y from L*y = b
     for i in range(size):
+        if abs(lu[i][i]) <= epsilon: print("Division by 0 at computing y from L*y=b, ("+str(i),str(i)+")"); return None
         y[i] = (b[i] - np.sum([lu[i][j] * y[j] for j in range(i)])) / lu[i][i]
     # compute x_lu from U*x_lu = y
     for i in reversed(range(size)):
@@ -59,12 +64,26 @@ def check_solution(a, b, x_lu):
     return np.linalg.norm(b - np.reshape(a.dot(x_lu), (len(x_lu), 1)))
 
 
-a = get_matrix_a("matrix_A.txt")
-b = get_matrix_b("matrix_B.txt")
-x_lu = compute_x_lu(len(b), 0, a, b)
+# import random
+# with open("matrix_C.txt","w") as f:
+#     f.write("100")
+#     f.write("\n")
+#     for i in range(100*100):
+#         f.write(str(random.randrange(-900,900)))
+#         f.write("\n")
+# with open("matrix_D.txt","w") as f:
+#     for i in range(100):
+#         f.write(str(random.randrange(-900,900)))
+#         f.write("\n")
+
+
+a = get_matrix_a("matrix_C.txt")
+b = get_matrix_b("matrix_D.txt")
+x_lu = compute_x_lu(len(b), a, b)
 x = np.reshape(np.linalg.inv(a).dot(b), len(b))
 print("Solution by inversion:", x)
 print("Solution x_lu:", x_lu)
 print("Frobenius norm b - A*x_lu:", check_solution(a, b, x_lu))
 print("Frobenius norm x_lu - x:", np.linalg.norm(x_lu - x))
 print("Frobenius norm x_lu - A^(-1)*b:", np.linalg.norm(x_lu - np.reshape(np.linalg.inv(a).dot(b), len(b))))
+
