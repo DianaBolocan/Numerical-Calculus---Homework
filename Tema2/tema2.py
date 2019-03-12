ef922c6a789d51fc5bbc440e5019da5b5dc13459
ClaudiuIoanMaftei <claudiu.ioan.maftei@gmail.com>
2019-03-12 17:10:12 +0200

    Update tema2.py

--- Tema2/tema2.py
+++ Tema2/tema2.py
@@ -13,6 +13,7 @@ def get_matrix_a(path):
                 size = int(line)
             else:
                 a.append(float(line))
+    a=np.array(a)
     return np.reshape(a, (size, size))
 
 
@@ -21,25 +22,43 @@ def get_matrix_b(path):
     with open(path, "r") as fd:
         for line in fd:
             b.append(float(line))
+    b=np.array(b)
     return np.reshape(b, (len(b), 1))
 
 
 def get_matrix_lu(size, a):
     lu = np.zeros((size, size))
+    lu=np.ones((size,size))
     for step in range(size):
-        i = 0
-        while i <= step:
-            if i > step - 1:
-                lu[step][i] = a[step][i] - np.sum([lu[step][k] if k == i else lu[step][k] * lu[k][i] for k in range(i)])
+        for i in range(step+1):
+            if i==step:
+                lu[step][i] = (a[i][step] - np.sum([lu[i][k] * lu[k][step] for k in range(i)])) / lu[i][i]
             else:
                 # compute elem col. step U
                 if abs(lu[i][i]) <= epsilon: print("Division by 0 at compute elem. col. step U, ("+str(i),str(i)+")"); return None
                 lu[i][step] = (a[i][step] - np.sum([lu[i][k] * lu[k][step] for k in range(i)])) / lu[i][i]
                 # compute elem. line step L
                 lu[step][i] = a[step][i] - np.sum([lu[step][k] if k == i else lu[step][k] * lu[k][i] for k in range(i)])
-            i += 1
+
     return lu
 
+def get_matrix_lu2(size,a):
+    lu = np.zeros((size, size))
+
+    for step in range(size):
+        if step>0:
+            for i in range(step):
+                s=0
+                for k in range(i):
+                    s+=lu[i][k]*lu[k][step]
+                lu[i][step]=(a[i][step]-s)/lu[i][i]
+        for i in range(step+1):
+            s=0
+            for k in range(i):
+                s+=lu[step][k]*lu[k][i]
+            lu[step][i]=a[step][i]-s
+
+    return lu
 
 def get_det_a(lu, size):
     return np.product([lu[i][i] for i in range(size)])
@@ -55,7 +74,7 @@ def compute_x_lu(size, a, b):
         y[i] = (b[i] - np.sum([lu[i][j] * y[j] for j in range(i)])) / lu[i][i]
     # compute x_lu from U*x_lu = y
     for i in reversed(range(size)):
-        x_lu[i] = y[i] - np.sum([lu[i][j] * y[j] for j in range(i+1, size)])
+        x_lu[i] = y[i] - np.sum([lu[i][j] * x_lu[j] for j in range(i+1, size)])
     return x_lu
 
 
@@ -66,19 +85,22 @@ def check_solution(a, b, x_lu):
 
 # import random
 # with open("matrix_C.txt","w") as f:
-#     f.write("100")
+#     f.write("3")
 #     f.write("\n")
-#     for i in range(100*100):
+#     for i in range(3*3):
 #         f.write(str(random.randrange(-900,900)))
 #         f.write("\n")
 # with open("matrix_D.txt","w") as f:
-#     for i in range(100):
+#     for i in range(3):
 #         f.write(str(random.randrange(-900,900)))
 #         f.write("\n")
 
 
 a = get_matrix_a("matrix_C.txt")
 b = get_matrix_b("matrix_D.txt")
+lu = get_matrix_lu2(len(b),a)
+print(a)
+print(lu)
 x_lu = compute_x_lu(len(b), a, b)
 x = np.reshape(np.linalg.inv(a).dot(b), len(b))
 print("Solution by inversion:", x)
