7a66b34b2f5c2456f68144cccadfa7e644d96eb6
Diana Bolocan <DianaBolocan@users.noreply.github.com>
2019-03-25 23:30:29 +0200

    Update homework3.py

--- Tema3/homework3.py
+++ Tema3/homework3.py
@@ -1,4 +1,5 @@
 import json
+import copy
 
 
 def to_json(file_name, matrix):
@@ -24,10 +25,47 @@ def extract_data(path):
                 elif column not in matrix[line].keys():
                     matrix[line][column] = value
                 else:
-                    matrix[line][column] = (matrix[line][column] + value) / 2
+                    matrix[line][column] += value
             elif input_line[0]:
                 vector.append(float(input_line[0]))
     return matrix, vector
+
+
+def addition(a, b):
+    result = copy.deepcopy(a)
+    for line in b.keys():
+        if line not in result.keys():
+            result[line] = b[line]
+        else:
+            for column in b[line].keys():
+                if column not in result[line].keys():
+                    result[line][column] = b[line][column]
+                else:
+                    result[line][column] += b[line][column]
+    return result
+
+
+def multiplication(a, b):
+    result = dict()
+    for line in a.keys():
+        for column in a[line].keys():
+
+    return result
+
+
+def same_matrices(a, b):
+    for line in a.keys():
+        if line not in b.keys():
+            print("no line {} in 2nd matrix".format(line))
+            return False
+        for column in a[line].keys():
+            if column not in b[line].keys():
+                print("no column {} in 2nd matrix".format(column))
+                return False
+            if a[line][column] != b[line][column]:
+                print("{} != {} at [{}][{}]".format(a[line][column], b[line][column], line, column))
+                return False
+    return True
 
 
 a, vector_a = extract_data("a.txt")
@@ -39,3 +77,11 @@ to_json("a", a)
 to_json("b", b)
 to_json("aplusb", aplusb)
 to_json("aorib", aorib)
+# a + b
+addition = addition(a, b)
+multiplication = multiplication(a, b)
+print(aorib)
+# to_json("aplusb", addition)
+# to_json("aorib", multiplication)
+# print(same_matrices(aplusb, addition))
+# print(same_matrices(aorib, multiplication))
