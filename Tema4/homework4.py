686a0498f8011860941708a1225f3b6aedf00c16
Diana Bolocan <DianaBolocan@users.noreply.github.com>
2019-04-16 10:58:50 +0300

    Update homework4
    There are still problems are x_sor computation.

--- Tema4/homework4.py
+++ Tema4/homework4.py
@@ -1,23 +1,88 @@
 from Homework3 import homework3
+import math
+import copy
+
+
+def format_x_sor(x_sor, size):
+    for line in range(size, 2*size):
+        del x_sor[line]
+    return x_sor
+
+
+def infinity_norm(x):
+    """
+    Compute infinity norm of x.
+    :param x: rare matrix represented as dictionary of dictionaries of non zero values
+    :return max_value: maximum value of x
+    """
+    max_value = None
+    for line in x.keys():
+        for column in x[line].keys():
+            if not max_value or max_value < x[line][column]:
+                max_value = x[line][column]
+    return max_value
 
 
 def check_values_on_diagonal(matrix):
+    """
+    Checks if a matrix made out of dictionary of dictionaries has values on diagonal
+    :param matrix: dictionary of dictionaries
+    :return: boolean
+    """
     for line in matrix.keys():
         if line not in matrix[line].keys():
             return False
     return True
 
 
-def get_x_sor(size, check_det, omega, max_interations=1000, epsilon=pow(10,-10)):
+def get_x_sor(a, b, size, check_det, omega, max_iterations=1000, epsilon=pow(10, -10)):
+    if omega >= 2 or omega <= 0:
+        print("[ERROR]: Omega should be in interval (0, 2).")
+        return None
     if check_det is False:
         print("[ERROR]: Cannot compute x using SOR.")
         return None
+    ok = False
     iterations = 0
-    x = homework3.create_x(size, 0)
-    return x, iterations
+    x_sor = homework3.create_x(2 * size, 0)
+    norm = 0
+    while iterations < max_iterations:
+        norm = 0
+        for line in range(size):
+            sum_1 = 0
+            sum_2 = 0
+            x_sor[line][0] += x_sor[line + size][0]
+            for column in a[line].keys():
+                if column < line:
+                    sum_1 += a[line][column] * x_sor[column][0]
+                else:
+                    sum_2 += a[line][column] * x_sor[column + size][0]
+            x_sor[line][0] += (omega / a[line][line]) * (b[line][0] - sum_1 - sum_2)
+            norm += pow(x_sor[line][0] - x_sor[line + size][0], 2)
+            x_sor[line + size][0] = copy.deepcopy(x_sor[line][0])
+        norm = math.sqrt(norm)
+        if norm < epsilon:
+            break
+        if norm > pow(10, 8):
+            break
+        iterations += 1
+    if norm < epsilon:
+        print("Approximated value: {}".format(x_sor))
+        ok = True
+    else:
+        print("[ERROR]: Divergence.")
+        if iterations >= max_iterations:
+            print("\t[WARNING]: Reached maximum iterations. Stopping the computation.\n\t[SUGGESTION]: Lower epsilon.")
+        if norm > pow(10, 8):
+            print(
+                "\t[WARNING]: Norm bigger than 10^8. Stopping the computation.\n\t[SUGGESTION]: Change omega or method.")
+            print("\tNorm:", norm)
+    print("Iterations: {}".format(iterations))
+    return (x_sor, ok)
 
 
 if __name__ == '__main__':
+    omegas = [0.8, 1.0, 1.2]
     for index in range(5):
         exec(
             "matrix_{}, vector_{}, size_{} = homework3.extract_data(\"m_rar_2019_{}.txt\")".format(index + 1, index + 1,
@@ -27,5 +92,13 @@ if __name__ == '__main__':
         exec("homework3.to_json(\"matrix_{}\", matrix_{})".format(index + 1, index + 1))
         exec("check_{} = check_values_on_diagonal(matrix_{})".format(index + 1, index + 1))
         exec("print(\"Values on diagonal for matrix {}:\", check_{})".format(index + 1, index + 1))
+        for index_omegas in range(len(omegas)):
+            exec("result_{} = get_x_sor(matrix_{}, vector_{}, size_{}, check_{}, {})".format(index_omegas, index + 1,
+                                                                                             index + 1, index + 1,
+                                                                                             index + 1,
+                                                                                             omegas[index_omegas]))
+            exec("if True in result_{}: print(infinity_norm(homework3.subtraction(homework3.multiplication(matrix_{}, "
+                 "format_x_sor(result_{}[0], size_{})), vector_{})))".format(index_omegas, index + 1, index_omegas,
+                                                                            index + 1, index + 1))
         print("-----------------------------------------------------------------------------------")
-    print(get_x_sor(size_1, check_1, 0.8))
+        input()
